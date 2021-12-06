e2aa4015ba00b0fb084c7598f2756fe2aabcc16c
Daniel Hladík <daniel@hladik.dev>
2021-12-06 18:55:51 +0100

    2021 - 06

---
+++ 2021/day_6/part_1.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+input = File.open('input.txt').read
+states = input.split(',').map(&:to_i)
+
+fishes = { 0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0 }
+
+states.each { |state| fishes[state] += 1 }
+
+80.times do
+  spawn_fish = fishes[0]
+
+  (1..8).each do |index|
+    fishes[index - 1] = fishes[index]
+  end
+
+  fishes[6] += spawn_fish
+
+  fishes[8] = spawn_fish
+end
+
+result = fishes.values.sum
+puts result
