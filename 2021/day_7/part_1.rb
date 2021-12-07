b9a2484cc0c7573569aa37aa14d901f578782f30
Daniel Hladík <daniel@hladik.dev>
2021-12-07 08:04:28 +0100

    2021 - 07

---
+++ 2021/day_7/part_1.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+input = File.open('input.txt').read
+input_positions = input.split(',').map(&:to_i)
+
+positions = Array.new(input_positions.max + 1, 0)
+input_positions.each { |position| positions[position] += 1 }
+
+result = Float::INFINITY
+
+positions.each_with_index do |_, move_to|
+  total_fuel_cost = 0
+
+  positions.each_with_index do |crabs, position|
+    fuel_cost = (position - move_to).abs
+    total_fuel_cost += fuel_cost * crabs
+  end
+
+  result = total_fuel_cost if total_fuel_cost < result
+end
+
+puts result
